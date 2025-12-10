b882235a9cc16adec6287046802ce8a4f6538fb0
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2025-12-10 20:12:32 +0700

    Login and Register Backend

---
+++ src/validations/userValidation.ts
@@ -0,0 +1,29 @@
+import { z, ZodType } from "zod"
+
+export class UserValidation {
+    static readonly REGISTER: ZodType = z.object({
+        username: z.string().min(3).max(30),
+        email: z.string().email(),
+        password: z.string().min(6).max(100),
+        bmigoal: z.number().min(10).max(50),
+        weight: z.number().positive(),
+        height: z.number().positive(),
+    })
+
+    static readonly LOGIN: ZodType = z.object({
+        email: z
+            .email({
+                error: "Email format is invalid!",
+            })
+            .min(1, {
+                error: "Email can not be empty!",
+            }),
+        password: z
+            .string({
+                error: "Password must be string!",
+            })
+            .min(8, {
+                error: "Password must contain more than or equal to 8 characters!",
+            }),
+    })
+}
